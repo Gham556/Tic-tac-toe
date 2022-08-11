19885e56e54b828f8dff5155af1fc654e0381011
George <georgea.fotheringham@gmail.com>
2022-08-11 13:47:11 -0400

    creates popop, adds initialize game function, adds add peice function

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-startButton = document.querySelector('button.startGame')
-startButton.addEventListener('click', enterPlayers);
+const startButton = document.querySelector('button.startGame');
+const initializeGame = document.querySelector('button.initializeGame')
 
-function enterPlayers() {
-    popUp.classlist.toggle('show');
+initializeGame.addEventListener('click', startGame);
+startButton.addEventListener('click', toggle);
+
+function toggle() {
+    const enterPlayers = document.querySelector('div.enterPlayers')
+    enterPlayers.classList.toggle('show');
 }
+
 
 function gameBoard() {
 let board = [a, b, c, d, e, f, g, h, i]}
@@ -17,18 +22,42 @@ function displayController() {
 }
 
 function startGame() {
-    const spaces = document.getElementsByClassName('space');
-    for (let space in spaces) {
+    toggle();
+    let playerOne = document.getElementsByName('playerOne').value;
+    let playerTwo = document.getElementsByName('playerTwo').value;
+    const spaces = document.querySelectorAll('div.space');
+    console.log(spaces);
+    for (let i = 0; i < spaces.length; i++) {
+          let  space = spaces[i];
         space.addEventListener('click', addPeice)};
-
+    return {playerOne, playerTwo};
 }
 
-function addPeice() {
-    if(player1) {
-        space.textContent = 'X';
+function addPeice(playerOne, playerTwo) {
+    console.log(this);
+    let round = [playerOne, playerTwo]
+    let currentRound = round[0]
+    if(currentRound) {
+        this.textContent = 'X';
+        switchPlayer(round);
     }
 
     else{
         space.textContent = 'O';
+        switchPlayer(round);
     }
 }
+
+function switchPlayer() {
+    
+    
+    if (currentRound === round[0]) {
+        currentRound = round[1];
+        alert('works')
+    }
+    else {
+        currentRound = round[0];
+        alert('also works')
+    }
+
+};
