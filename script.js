f5526ffbc74678fc5f7f8a0c58b4fab8171f93f1
George <georgea.fotheringham@gmail.com>
2022-08-12 20:31:09 -0400

    adds player name UI, attempts to create game end function
    The game end function attempts to check the text content of each square and compare it to a matching array of the winning space
    combinations. I have been unable to find a way to get the text content of each div when they are the value pairs of an array.
    
    The solution I have in mind is to scrap the gameBoard object creator and send the div.space items directly into an array, to remove the issue of iterating through several objects and then attempting to target the textContent of each value.
    I have avoided  using class names because I didn't want to remove the ability to generate a larger board in the future. The solution to this may be changing the class of each element on click as well as the text content.
    That way the programs still maintains flexibility.

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const startButton = document.querySelector('button.startGame');
-const initializeGame = document.querySelector('button.initializeGame')
+const initializeGameButton = document.querySelector('button.initializeGame')
+const playerOneName = document.getElementById('playerOne');
+const playerTwoName = document.getElementById('playerTwo');
+const nameOne = document.querySelector('div.nameOne');
+const nameTwo = document.querySelector('div.nameTwo');
+const gameArea = document.querySelector('div.popup')
+const spaces = document.querySelectorAll('div.space');
 
-initializeGame.addEventListener('click', startGame);
+initializeGameButton.addEventListener('click', initializeGame);
 startButton.addEventListener('click', toggle);
 
 function toggle() {
     const enterPlayers = document.querySelector('div.enterPlayers')
     enterPlayers.classList.toggle('show');
 }
-
 
 function gameBoard(a, b, c, d, e, f, g, h, i) {
 this.a = a;
@@ -20,34 +25,40 @@ this.f = f;
 this.g = g;
 this.h = h;
 this.i = i;
+    
+    winningboards = [{a, b, c}, {d, e, f}, {g, h, i}, {a, d, g}, {b, e, h}, {c, f, i}, {a, e, i}, {c, e, g}]  
+    return {winningboards};
+
 }
 
 function players(name) {
     this.name = name;
 }
 
-function displayController() {
 
-}
-
-function startGame() {
+function initializeGame() {
     toggle();
-    const spaces = document.querySelectorAll('div.space');
-    console.log(spaces);
+    playerOne = new players(playerOneName.value);
+    playerTwo = new players(playerTwoName.value)
+    nameOne.textContent = playerOne.name;
+    nameTwo.textContent = playerTwo.name;
+    console.log(playerTwo)
     let allSpacesArray = [];
     let round = [0,1];
+   
     for (let i = 0; i < spaces.length; i++) {
-          space = spaces[i];
-          allSpacesArray.push(space);
-        space.addEventListener('click', function (e){
+            space = spaces[i];
+            allSpacesArray.push(space);
+            space.addEventListener('click', function (e){
             console.log(this);
-            console.log(e);
-            
+            console.log(toggle);
             let currentRound = round[round.length - 1];
             if (!this.textContent) {
                     if (currentRound) {
-                        this.textContent = 'X';
+                        this.textContent = 'X';          
                         round.push(0);
+
+                        }
                     }
         
                     else {
@@ -58,11 +69,18 @@ function startGame() {
             })};
         
        
-    const allSpaces = new gameBoard(allSpacesArray[0],allSpacesArray[1],allSpacesArray[2], allSpacesArray[3], allSpacesArray[4], allSpacesArray[5], allSpacesArray[6], allSpacesArray[7], allSpacesArray[8])
-    console.log (allSpaces)
-    return allSpaces
+    const allSpaces = new gameBoard(allSpacesArray[0],allSpacesArray[1],allSpacesArray[2], allSpacesArray[3], allSpacesArray[4], allSpacesArray[5], allSpacesArray[6], allSpacesArray[7], allSpacesArray[8]) 
+        
+        console.log(winningboards)
     
 }
 
-
-
+let runGame = function () {
+    for (let i = 0; i < spaces.length; i++) {
+    space = spaces[i];
+    space.addEventListener('DOMCharacterDataModified', function(){
+    console.log(winningboards);
+    console.log('works')
+})
+};
+}();
